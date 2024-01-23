684ce4bcbc9f434ffdade161b33695a63261915e
NanaOdamme <eronastyles@gmail.com>
2024-01-23 13:22:13 +0000

    update

--- Snake Game/script.js
+++ Snake Game/script.js
@@ -1,1 +1,18 @@
-console.log("test");
+// define html elements
+const board = document.getElementById('game-board');
+
+//define game variants
+let snake = [{x: 10, y: 10}]
+
+//function to draw game map
+function draw()
+{
+    board.innerHTML = '';
+    drawSnake{}
+}
+
+//draw snake
+function drawSnake()
+{
+
+}
