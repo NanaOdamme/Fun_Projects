4d2cfcabe05eb36bde639bbb2259bb3a77c43e08
NanaOdamme <eronastyles@gmail.com>
2024-01-30 09:40:00 +0000

    updated

--- Snake Game/script.js
+++ Snake Game/script.js
@@ -8,11 +8,27 @@ let snake = [{x: 10, y: 10}]
 function draw()
 {
     board.innerHTML = '';
-    drawSnake{}
+    drawSnake();
 }
 
 //draw snake
 function drawSnake()
 {
+    snake.forEach((segment) => {
+        const snakeElement = createGameElement('div',
+        'snake');
+        setPosition(snakeElement, segment);
+    });
+}
 
+//create a snake or food cube
+function createGameElement(tag, className) {
+    const element = document.createElement(tag);
+    element.className = className;
+    return element;
+}
+
+// set the position of the snake or food
+function setPosition(element, position) {
+    element.style.gridColumn = position.x;
 }
